8d50e57f596553a8184cec006fd08706d09b9abf
Yomira Martinez <yomimsm@gmail.com>
2024-10-29 19:23:51 -0600

    For 2

---
+++ Encontrarnumero/src/encontrarnumero/Encontrarnumero.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package encontrarnumero;
+
+import java.util.Scanner;
+public class Encontrarnumero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresa un numero: ");
+        num1 = teclado.nextInt();
+        System.out.println("Ingresa un numero: ");
+        num2 = teclado.nextInt();
+        System.out.println("Ingresa un numero: ");
+        num3 = teclado.nextInt();
+        for (int i = num1; i <= num2; i++) {
+            if(i == num3){
+                System.out.println("verdadero");
+                break;
+            }
+            
+        }
+    }
+    
+    
+    
+}
---
+++ impares1al100/src/impares1al100/Impares1al100.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package impares1al100;
+
+/**
+ *
+ * @author Yomi
+ */
+public class Impares1al100 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if(i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
